33dd590cafc7c74fd292ead1f01ae3df8a857536
alexhsieh <alexhsieh@awoo.com.tw>
2025-06-09 08:15:31 +0800

    change path

--- cloud-ai/aws/startup.sh
+++ cloud-ai/aws/startup.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-# --- VARIABLES ---
-REPO_URL="https://github.com/qwedsazxc78/ai-automation-n8n.git"
-BASE_DIR="${HOME}/ai-automation-n8n"
-COMPOSE_PATH="${BASE_DIR}/local-ai/aws"
 
 # --- 0. PREP: install git ---
 sudo apt-get update -y
@@ -12,8 +8,8 @@ sudo apt-get install -y git apt-transport-https ca-certificates curl software-pr
 
 # --- 1. INSTALL DOCKER ENGINE ---
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-cache policy docker-ce
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
 sudo apt install -y docker-ce
 
 # --- 2. ENABLE & START ---
@@ -26,6 +22,10 @@ sudo usermod -aG docker $USER
 sudo systemctl enable docker
 sudo systemctl start docker
 
+# --- VARIABLES ---
+REPO_URL="https://github.com/qwedsazxc78/ai-automation-n8n.git"
+BASE_DIR="${HOME}/ai-automation-n8n"
+COMPOSE_PATH="${BASE_DIR}/local-ai/aws"
 
 # --- 3. CLONE OR UPDATE YOUR REPO ---
 if [ -d "${BASE_DIR}" ]; then
